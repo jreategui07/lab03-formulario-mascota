ca0e1684b6ccab6845776070c7404e3dda265859
Jonathan Reátegui <j.reategui2012@gmail.com>
2021-10-19 00:39:38 -0500

    Adding inputs validations

--- app/src/main/java/com/example/lab03_formulario_mascota/MainActivity.kt
+++ app/src/main/java/com/example/lab03_formulario_mascota/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.lab03_formulario_mascota
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -29,6 +30,17 @@ class MainActivity : AppCompatActivity() {
             val hasLeptospiriosisVaccine: Boolean = chkLeptospiriosis.isChecked
             val hasRabiaVaccine: Boolean = chkRabia.isChecked
 
+
+            if(petName.isEmpty()) {
+                toast("Debe ingresar el nombre de la mascota")
+                return@setOnClickListener
+            }
+
+            if(petAge.isEmpty()) {
+                toast("Debe ingresar la edad de la mascota")
+                return@setOnClickListener
+            }
+
             println(petName)
             println(petAge)
             println(petType)
@@ -41,4 +53,6 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    fun toast(mensaje: String) = Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
 }
